fe05177de5b18fb47090aa599df923150cbae491
Celybot <youancece@gmail.com>
2022-11-30 10:49:38 -0500

    added components for home page

---
+++ homeful/src/Body.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Body.css'
+import Mission from './Mission'
+
+function Body(){
+  return (
+    <body className='body'>  
+    <Mission/>
+    {/* <img src="https://www.w3schools.com/images/w3schools_green.jpg" alt="holding hands" width="300px" height="300px"/> */}
+    {/* style={{ backgroundImage: `url(./landingpage_handshome.png)`}} */}
+        {/* <section className='mission'>
+            <h1 className='discription'> Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+            </h1>
+            <button className = 'button' type="button">Sign Up</button>
+         </section> */}
+    </body>
+    )
+}
+
+export default Body
