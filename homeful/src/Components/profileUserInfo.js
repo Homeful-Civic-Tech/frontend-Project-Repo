3779af586211b66583f83fa1eff656cefe3743bd
Celybot <youancece@gmail.com>
2022-12-12 21:55:26 -0500

    made username render to screen

--- homeful/src/Components/profileUserInfo.js
+++ homeful/src/Components/profileUserInfo.js
@@ -1,16 +1,12 @@
 import React from "react"
 
 
-let Users = [{id:"111111A", firstname:"Baron", lastname:"Renzik", username: "DaBest", email:"Baron@gmail.com", sex:"Female", password: "IloveKittes123", bio: "I'm feeling good about myself cant complain"},
-// {id:"111111B", firstname:"karen", lastname:"hernandez", username: "karebear2000", email:"788Baron@gmail.com", sex:"Female", password: "IamD#1PASS", bio:"im feeling LUCKY"}
-]
+let Users = [{"username": localStorage.getItem("username")}]
 
 function Profiles(prop){
     return(
     <>
-    <div>{console.log(prop)}</div>
      <h1>{prop.obj.username}</h1>
-     <h3>{prop.obj.bio}</h3>
     </>
     )
 }
